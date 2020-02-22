714cfb03e804610413dca1a5688878d5805879be
Christian Söderberg <christian.v.soderberg@gmail.com>
2020-02-22 18:20:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 echo "Creating an SSH key for you..."
 ssh-keygen -t rsa
 
-echo "Please add this public key to Github \n"
-echo "https://github.com/account/ssh \n"
+echo "Please add this public key to Github"
+echo "https://github.com/account/ssh"
 read -p "Press [Enter] key after this..."
 
 # Check for Homebrew,
