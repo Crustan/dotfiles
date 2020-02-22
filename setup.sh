b5172942b74c13292cc2dceb88e0902e612b5820
Christian Söderberg <christian.v.soderberg@gmail.com>
2020-02-22 18:17:48 +0100

    Update setup with confirmation after ssh step

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 echo "Creating an SSH key for you..."
 ssh-keygen -t rsa
 
+echo "Please add this public key to Github \n"
+echo "https://github.com/account/ssh \n"
+read -p "Press [Enter] key after this..."
+
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
