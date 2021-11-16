b9f47b53c673cf32ab5293ca684ebf20a5e28811
Christian Söderberg <christian.soderberg@paradoxinteractive.com>
2021-11-16 17:13:46 +0100

    Refactor setup script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update homebrew recipes
@@ -17,64 +17,40 @@ echo "Updating homebrew..."
 brew update
 
 # Install git
+echo "Installing git..."
 brew install git
 git config --global user.name "Christan Söderberg"
 git config --global user.email christian.v.soderberg@gmail.com
+git config --global fetch.prune true
+git config --global push.default current
 
-echo "Copying dotfiles from Github"
-cd ~
-git clone git@github.com:Crustan/dotfiles.git .dotfiles
-cd .dotfiles
+echo "Copying dotfiles from Github..."
+git clone git@github.com:Crustan/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+
+# source setup/link.sh
 
 # Install Homebrew bundle
 brew bundle
 cd ~
 
 # Install Zsh & Oh My Zsh
-echo "Installing Oh My ZSH..."
-curl -L http://install.ohmyz.sh | sh
-
 echo "Setting ZSH as shell..."
 chsh -s /bin/zsh
 
-#"Disabling OS X Gate Keeper"
-#"(You'll be able to install any app you want from here on, not just Mac App Store apps)"
-sudo spctl --master-disable
-sudo defaults write /var/db/SystemPolicy-prefs.plist enabled -string no
-defaults write com.apple.LaunchServices LSQuarantine -bool false
+# echo "Installing Oh My ZSH..."
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir -p ~/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/reobin/typewritten.git ~/.zsh/typewritten
 
-#"Don’t automatically rearrange Spaces based on most recent use"
-defaults write com.apple.dock mru-spaces -bool false
+echo "Installing NVM..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
-#"Automatically quit printer app once the print jobs complete"
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+echo "Installing Node..."
+nvm install node
 
-#"Enabling subpixel font rendering on non-Apple LCDs"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+source setup/macos.sh
 
-#"Showing all filename extensions in Finder by default"
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-#"Use column view in all Finder windows by default"
-defaults write com.apple.finder FXPreferredViewStyle Clmv
-
-#"Setting screenshots location to ~/Desktop"
-defaults write com.apple.screencapture location -string "$HOME/Desktop"
-
-#"Setting screenshot format to PNG"
-defaults write com.apple.screencapture type -string "png"
-
-#"Enabling the Develop menu and the Web Inspector in Safari"
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-
-#"Hide the donate message"
-defaults write org.m0k.transmission WarningDonate -bool false
-
-#"Hide the legal disclaimer"
-defaults write org.m0k.transmission WarningLegal -bool false
-
-killall Finder
-
-echo "Done!"
+echo "Done! Reload terminal"
