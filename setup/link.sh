9d5537ff90ac0843b927578d2ea1b342db39a9b7
Christian Söderberg <christian.soderberg@paradoxinteractive.com>
2021-11-17 11:13:28 +0100

    Change symlink for zshrc

--- setup/link.sh
+++ setup/link.sh
@@ -2,18 +2,16 @@
 
 DOTFILES=$HOME/.dotfiles
 
-echo -e "\nCreating symlinks"
+echo -e "\nCreating symlink for .zshrc"
 echo "=============================="
-linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
-for file in $linkables ; do
-    target="$HOME/.$( basename $file '.symlink' )"
-    if [ -e $target ]; then
-        echo "~${target#$HOME} already exists... Skipping."
-    else
-        echo "Creating symlink for $file"
-        ln -s $file $target
-    fi
-done
+target="$HOME/.zshrc"
+file="$DOTFILES"/zsh/.zshrc
+if [ -e $target ]; then
+    echo "~${target#$HOME} already exists... Skipping."
+else
+    echo "Creating symlink for $file"
+    ln -s $file $target
+fi
 
 
 echo -e "\n\nInstalling to ~/.config"
