7436103019daedf4f5543c704613f0c0545f7eb0
Christian Söderberg <christian.soderberg@paradoxinteractive.com>
2021-11-17 11:20:55 +0100

    Add symlink for hot keys

--- setup/link.sh
+++ setup/link.sh
@@ -2,18 +2,6 @@
 
 DOTFILES=$HOME/.dotfiles
 
-echo -e "\nCreating symlink for .zshrc"
-echo "=============================="
-target="$HOME/.zshrc"
-file="$DOTFILES"/zsh/.zshrc
-if [ -e $target ]; then
-    echo "~${target#$HOME} already exists... Skipping."
-else
-    echo "Creating symlink for $file"
-    ln -s $file $target
-fi
-
-
 echo -e "\n\nInstalling to ~/.config"
 echo "=============================="
 if [ ! -d $HOME/.config ]; then
@@ -30,3 +18,24 @@ for config in $DOTFILES/config/*; do
         ln -s $config $target
     fi
 done
+
+echo -e "\nCreating symlink for .zshrc"
+echo "=============================="
+zshtarget=$HOME/.zshrc
+if [ -e $zshtarget ]; then
+    echo "~${zshtarget#$HOME} already exists... Skipping."
+else
+    echo "Creating symlink for .zshrc"
+    ln -s $file $DOTFILES/zsh/.zshrc $zshtarget
+fi
+
+echo -e "\nCreating symlink for hot keys"
+echo "=============================="
+hktarget=$HOME/Library/Preferences/com.apple.symbolichotkeys.plist
+hkfile=$DOTFILES/com.apple.symbolichotkeys.plist
+if [ -e $hktarget ]; then
+    echo "~${hktarget#$HOME} already exists... Skipping."
+else
+    echo "Creating symlink for $hkfile"
+    ln -s $hkfile $hktarget
+fi
