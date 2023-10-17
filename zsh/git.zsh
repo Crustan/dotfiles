693ef5592d3bb761cc17ced4347678051bbe83bf
Christan Söderberg <christian.v.soderberg@gmail.com>
2023-10-17 17:13:07 +0200

    Add more git aliases

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -2,6 +2,7 @@
 alias g-='git checkout -'
 alias gc='git checkout'
 alias gcm='git commit -m'
+alias gca='git commit --amend --no-edit'
 alias ga='git add'
 alias gb='git branch'
 alias gl='git pull'
@@ -15,5 +16,8 @@ alias gmv='git mv'
 alias grm='git rm'
 alias grn='git-rename'
 alias glog="git l"
-alias gcleanup='git branch --v | grep "\[gone\]" | awk '\''{print $1}'\'' | xargs git branch -D'
+alias gundo="git reset --soft HEAD~1"
+alias gdb="git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -D"
+alias gclean='git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
+alias gcleandry='git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && echo "$branch is merged into main and can be deleted"; done'
 alias git-undo='git reset --soft HEAD~1'
