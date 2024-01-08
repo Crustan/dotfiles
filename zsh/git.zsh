e303b591ed0d3a15c4b356d4797fb0bc681d9922
Christan Söderberg <christian.v.soderberg@gmail.com>
2024-01-08 16:22:32 +0100

    Formatting

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -15,9 +15,9 @@ alias gsp='git stash pop'
 alias gmv='git mv'
 alias grm='git rm'
 alias grn='git-rename'
-alias glog="git l"
-alias gundo="git reset --soft HEAD~1"
-alias gdb="git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -D"
+alias glog='git l'
+alias gundo='git reset --soft HEAD~1'
+alias gdb='git branch -vv | grep ": gone]"|  grep -v "\*" | awk "{ print $1; }" | xargs -r git branch -D'
 alias gclean='git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
 alias gcleandry='git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && echo "$branch is merged into main and can be deleted"; done'
 alias git-undo='git reset --soft HEAD~1'
