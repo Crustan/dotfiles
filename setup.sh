714a48749e1c5d9bb3be320cdea826e414c10844
Christian Söderberg <christian.v.soderberg@gmail.com>
2026-01-28 15:43:13 +0100

    fix: home path

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ if ! command -v brew &> /dev/null; then
   echo -e "\n\nInstalling homebrew"
   echo "=============================="
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~HOME/.zprofile
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
